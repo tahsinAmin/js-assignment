264ede3dec60780189a953700baa3e3c5d9a0f74
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2021-12-27 19:04:36 +0600

    search bar demo works

--- js/search_script.js
+++ js/search_script.js
@@ -6,12 +6,40 @@ const suggBox = document.querySelector(".autocom-box ");
 // if user press any key and release
 inputBox.onkeyup = (e) => {
     let userData = e.target.value; // user enetered data
-    let emptyArray = suggestions.filter(data => {
-        return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-    });
-    emptyArray = emptyArray.map(data => {
-        return data = '<li>'+ data +'</li>';
-    });
-    console.log(emptyArray);
+    let emptyArray = []
+    if(userData){
+        emptyArray = suggestions.filter(data => {
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map(data => {
+            return data = '<li>'+ data +'</li>';
+        });
+        console.log(emptyArray);
+        searchWrapper.classList.add("active");
+        showSuggestions(userData, emptyArray);
+        let altList = suggBox.querySelectorAll("li");
+        for(let i =0;i< altList.length;i++){
+            altList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active");
+    }
+   
 }
- 
+
+function select(element){
+    let selectUserData = element.textContent;
+    inputBox.value = selectUserData;
+    searchWrapper.classList.remove("active");
+}
+
+function showSuggestions(userData, list) {
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData='<li>'+ userData +'</li>'; 
+    }else{
+        listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
